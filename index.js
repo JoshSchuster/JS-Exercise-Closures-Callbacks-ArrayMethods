6fb99f2ce717941f60dcf8961efcbb15a56c0f2e
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-10-01 00:23:43 -0500

    completes task 2

--- index.js
+++ index.js
@@ -70,11 +70,20 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(team){
+  return function(){
+    //generates random, whole number between 0 and 2
+    const points = Math.floor(Math.random()*3);
+    console.log(`${team} scored ${points} points this inning.`);
+  }
 }
+
+const homeTeamInningScore = inning("Brewers"); //test
+
+homeTeamInningScore(); //test
+homeTeamInningScore(); //test
+homeTeamInningScore(); //test
+homeTeamInningScore(); //test
 
 /* Task 3: finalScore()
 
